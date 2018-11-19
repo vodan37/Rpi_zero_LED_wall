6bd30147d6e7105d3cb67519353bf31f4ca32ea6
vodan37 <vodan37@mail.ru>
2018-11-19 12:46:20 +0500

    Add files via upload

--- frame_handler.py
+++ frame_handler.py
@@ -289,20 +289,21 @@ class Tetris(Handler):
                         self.matrix[str][col][0] = False
                         self.matrix[str][col][1] = self.color
                     else:
-                        if (str < self.columns[col]):
-                            self.matrix[str+1][col][0] = False
-                            self.matrix[str+1][col][1] = self.color
-                            self.matrix[str+1][col][2] = current_time+1
-                            self.matrix[str][col][1] = self.backgroud
-                            self.matrix[str][col][0] = True
-                        else:
-                            self.matrix[str+1][col][0] = False
-                            self.matrix[str+1][col][1] = self.color
-                            self.matrix[str+1][col][2] = current_time+1
-                            self.matrix[str][col][0] = False
-                            self.matrix[str][col][1] = self.color
-                            self.matrix[str][col][2] = current_time+1
-                            self.columns[col] = self.columns[col] + 1
+                        if (str < 14):
+                            if (str < self.columns[col]):
+                                self.matrix[str+1][col][0] = False
+                                self.matrix[str+1][col][1] = self.color
+                                self.matrix[str+1][col][2] = current_time+1
+                                self.matrix[str][col][1] = self.backgroud
+                                self.matrix[str][col][0] = True
+                            else:
+                                self.matrix[str+1][col][0] = False
+                                self.matrix[str+1][col][1] = self.color
+                                self.matrix[str+1][col][2] = current_time+1
+                                self.matrix[str][col][0] = False
+                                self.matrix[str][col][1] = self.color
+                                self.matrix[str][col][2] = current_time+1
+                                self.columns[col] = self.columns[col] + 1
 
 
         self.frame = []
